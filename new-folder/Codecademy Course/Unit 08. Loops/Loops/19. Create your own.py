e910bf1d299f5bd3e89992a07ab8f6c1604f6d43
Saburka <kasiasabura@gmail.com>
2017-06-17 23:18:59 +0200

    Create 19. Create your own.py

---
+++ new-folder/Codecademy Course/Unit 08. Loops/Loops/19. Create your own.py
@@ -0,0 +1,8 @@
+countries = ["Germany", "France", "Spain", "Rome", "Greece"]
+
+print "The countries I want visit this year are:"
+for country in countries:
+    if country == "Rome":
+        print "Rome is  not a country!"
+    else:
+        print country, "it's a nice country to visit"
